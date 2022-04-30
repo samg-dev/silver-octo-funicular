970b142c1738ee4c4060e6136f485200578ff632
Samuel Garnham <1120118@my.brunel.ac.uk>
2022-04-30 11:27:35 +0100

    Adding some templates, bootstrap css

--- views.py
+++ views.py
@@ -4,21 +4,33 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template("index.html", name="Tim")
+    return render_template("index.html", name="Sam")
 
-@views.route("/profile")
-def profile():
-    return render_template("profile.html")
+@views.route("/sfx")
+def sfx():
+    return render_template("sfx.html")
 
-@views.route("/json")
-def get_json():
-    return jsonify({"username": "Sam"})
+@views.route("/gifs")
+def gifs():
+    return render_template("gifs.html")
 
-@views.route("/data")
-def get_data():
-    data = request.json
-    return jsonify(data)
+# @views.route("/profile")
+# def profile():
+#     return render_template("profile.html")
 
-@views.route("/go-to-home")
-def go_to_home():
-    return redirect(url_for("views.home"))
+# @views.route("/json")
+# def get_json():
+#     return jsonify({"username": "Sam"})
+
+# @views.route("/data")
+# def get_data():
+#     data = request.json
+#     return jsonify(data)
+
+# @views.route("/go-to-home")
+# def go_to_home():
+#     return redirect(url_for("views.home"))
+
+# @views.route("/profile/<name>")
+# def named_profile(name):
+#     return render_template("profile.html", name=name)
